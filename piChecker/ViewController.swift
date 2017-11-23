7a1d53aaa2fa6ff7b784aa6c989ba7ba2ced6ad9
Katherine McFerrin <32803248+katmcferrin@users.noreply.github.com>
2017-11-23 11:24:14 -0500

    checks the digits of Pi

---
+++ piChecker/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  piChecker
+//
+//  Created by Katherine McFerrin on 11/20/17.
+//  Copyright © 2017 Katherine McFerrin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var piCollection: UITextField!
+    
+    @IBAction func checkAction(_ sender: Any) {
+        
+        if (piCollection.text == "") {
+            let alertController = UIAlertController(title: "Error", message:"Please type the digits of PI", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+        
+           present(alertController, animated: true, completion: nil)
+       }
+        else {
+            if (piCollection.text == "3.1415926535897932384626433")
+            {
+                let alertController = UIAlertController(title: "YAY", message: "You did it!", preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "Nice job!", style: .cancel, handler: nil)
+                
+                present(alertController, animated: true, completion: nil)
+            }
+        else {
+            let alertController = UIAlertController(title: "Hmmm...", message: "We're not quite sure what happened, lease try again!", preferredStyle:. alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                
+            alertController.addAction(defaultAction)
+                
+            present(alertController, animated:true, completion:nil)
+        }
+    }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
